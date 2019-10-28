dda78a54a930264e0478e8e96034c87661991dfe
Nicola Di Marco <dimarco.nicola@gmail.com>
2019-10-28 17:24:30 +0100

    origin

---
+++ src/pages/_app.js
@@ -0,0 +1,35 @@
+import React from "react";
+import IndexPage from "./index";
+import { Switch, Route, Router } from "./../util/router.js";
+import "./../util/analytics.js";
+import { ProvideAuth } from "./../util/auth.js";
+
+function App(props) {
+  return (
+    <ProvideAuth>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={IndexPage} />
+
+          <Route
+            component={({ location }) => {
+              return (
+                <div
+                  style={{
+                    padding: "50px",
+                    width: "100%",
+                    textAlign: "center"
+                  }}
+                >
+                  The page <code>{location.pathname}</code> could not be found.
+                </div>
+              );
+            }}
+          />
+        </Switch>
+      </Router>
+    </ProvideAuth>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,61 @@
+import React from "react";
+import Navbar from "./../components/Navbar";
+import HeroSection from "./../components/HeroSection";
+import ClientsSection from "./../components/ClientsSection";
+import TestimonialsSection from "./../components/TestimonialsSection";
+import ContactSection from "./../components/ContactSection";
+import Footer from "./../components/Footer";
+import { useRouter } from "./../util/router.js";
+
+function IndexPage(props) {
+  const router = useRouter();
+
+  return (
+    <>
+      <Navbar
+        color="white"
+        spaced={true}
+        logo="https://uploads.divjoy.com/logo.svg"
+      />
+      <HeroSection
+        color="white"
+        size="medium"
+        title="Your landing page title here"
+        subtitle="This landing page is perfect for showing off your awesome product and driving people to sign up for a paid plan."
+        buttonText="Start Free Trial"
+        buttonOnClick={() => {
+          router.push("/pricing");
+        }}
+      />
+      <ClientsSection
+        color="white"
+        size="medium"
+        title="You're in good company"
+        subtitle=""
+      />
+      <TestimonialsSection
+        color="white"
+        size="medium"
+        title="Here's what people are saying"
+        subtitle=""
+      />
+      <ContactSection
+        color="white"
+        size="medium"
+        title="Contact Us"
+        subtitle=""
+        showNameField={true}
+        buttonText="Send message"
+      />
+      <Footer
+        color="white"
+        size="medium"
+        logo="https://uploads.divjoy.com/logo.svg"
+        description="A short description of what you do here"
+        copyright="© 2019 Company"
+      />
+    </>
+  );
+}
+
+export default IndexPage;
